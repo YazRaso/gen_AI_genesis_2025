7910255c1c9a23399932759fe065bf8d368b1cb9
Sean Zhang <73952098+MerpDurp@users.noreply.github.com>
2025-03-22 16:18:41 -0400

    Made main.py read entire pdf

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,32 @@
 import os
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 
 # Configure the API key
 # in bash: `export GEMINI_API_KEY=[key]`
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")  # Set your API key in environment variables
-genai.configure(api_key=GEMINI_API_KEY)
+client = genai.Client(api_key='AIzaSyAUWuyUe5Hicxeqz_xHNFMG-kgYOd32EvY')
 
 # Create the Generative Model instance
-model = genai.GenerativeModel('gemini-2.0-flash')
+model = 'gemini-2.0-flash'
 
 def generate_content(prompt):
     try:
-        response = model.generate_content(prompt)
+        response = client.models.generate_content(model, prompt)
+        return response.text
+    except Exception as e:
+        return f"Error generating content: {str(e)}"
+
+def get_content(learn_file):
+    text = "You are a high school level tutor. Summarize the following content into easy to follow core concepts."
+    try:
+        response = client.models.generate_content(model=model, contents=[text, learn_file])
         return response.text
     except Exception as e:
         return f"Error generating content: {str(e)}"
 
 # Example usage
 if __name__ == "__main__":
-    prompt = "Write a short poem about technology and nature"
-    result = generate_content(prompt)
+    file = client.files.upload(file='biology-student-textbook-grade-9_cell_biology.pdf')
+    result = get_content(file)
     print(result)
