a12811a0d0f4314236d4c4be7f688aa0f572bd1a
Sean Zhang <73952098+MerpDurp@users.noreply.github.com>
2025-03-22 22:46:34 -0400

    Finished Quiz and Concept classes

--- backend/quiz.py
+++ backend/quiz.py
@@ -3,20 +3,22 @@ from google import genai
 
 from main import CLIENT, MODEL
 
+# For testing cause no clue how to get answers
 TEST_ANSWERS = ["Midocondira?", "Cell formations?", "I don't understand"]
-TEST_CONCEPTS = ["Cell Formation", "Cell Transportation"]
+
+# Replace with actual summary file
+with open("summary.txt", 'r') as f:
+    SUMMARY = f.read()
 
 class Concept:
     name: str
     questions: list
     learned: bool
-    summary: str
 
-    def __init__(self, name: str, con_sum: str) -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
         self.learned = False
         self.questions = []
-        self.summary = con_sum
 
     def check_answer(self, question_index: int, answer: str) -> (bool, str):
         text = ("You are a high school tutor. The following answer to the question "
@@ -26,7 +28,8 @@ class Concept:
                 "for incorrect answers followed by an explanation why:\n")
         text += "Question: " + self.questions[question_index] + "\n"
         text += "Student's Answer: " + answer + "\n"
-        text += "Summary: " + self.summary + "\n"
+        text += "Summary: " + SUMMARY + "\n"
+        # TODO Util here
         response = CLIENT.models.generate_content(model=MODEL, contents=text)
         if response.text == 'Correct':
             return True, "Correct"
@@ -34,61 +37,63 @@ class Concept:
             return False, response.text
 
     def generate_questions(self) -> None:
-        # Generate 3 questions based on concept
+        # Generate 3 new questions based on concept
+        self.questions = []
         for i in range(3):
-            text = "You are a high school tutor. Create a quiz question for this core concept that is different from the other questions in the attached list. Return your response so it is only the question.:\n"
+            text = ("You are a high school tutor. Create a quiz question for this "
+                    "core concept that is different from the other questions in "
+                    "the attached list. Return your response so it is only the question.:\n")
             text += "Concept: " + self.name + "\n"
-            text += "Summary: " + self.summary + "\n"
+            text += "Summary: " + SUMMARY + "\n"
             text += "Previous Questions: " + str(self.questions)
+            # TODO Util here
             response = CLIENT.models.generate_content(model=MODEL, contents=text)
             self.questions.append(response.text)
 
+    def check_concept(self) -> list[list[int, str]]:
+        outcomes = []
+        for i in range(3):
+            outputs = self.check_answer(i, TEST_ANSWERS[i])
+            if not outputs[0]:
+                outcomes.append([i, outputs[1]])
+        if len(outcomes) == 0:
+            self.learned = True
+        return outcomes
 
-def check_concept(concept: Concept) -> bool | str:
-    for i in range(3):
-        result = concept.check_answer(i, TEST_ANSWERS[i])
-        if not result[0]:
-            return "You got the following question incorrect: " + concept.questions[i] + result[1]
-    concept.learned = True
-    return True
+# Concepts for testing
+TEST_CONCEPTS = [Concept("Cell Formation"),Concept("Cell Transportation")]
 
+class Quiz:
+    failed_cons: list[Concept]
+    questions: list[str]
+    wrong_ques: dict[int: str]
 
-class Lesson:
-    folder: str
-    _core_concepts: list[Concept]
-    summary: str
-    learned: bool
+    def __init__(self) -> None:
+        self.failed_cons = []
+        self.wrong_ques = {}
+        self.questions = []
+        for concept in TEST_CONCEPTS:
+            concept.generate_questions()
+        for concept in TEST_CONCEPTS:
+            self.questions.extend(concept.questions)
 
-    def __init__(self, folder: str, c: Concept) -> None:
-        self._folder = folder
-        self._core_concepts = [c]
-        # Code below is used for getting concepts from a file
-        # f = open(os.path.join(folder, "concepts.txt"), "r")
-        # self._core_concepts = f.readlines()
-        """
-        self.questions = {}
-        for i in range(len(self._core_concepts)):
-            concept = self._core_concepts[i].strip()
-            self.questions[concept] = []
-            """
-
-    # Checks if they got the lesson correct (All concepts are right)
-    # def check_lesson(self) -> None:
-    #     for concept in self._core_concepts:
-    #         if not concept.learned:
-    #             if isinstance(check_concept(concept), str):
-
-
+    def quiz_user(self) -> None:
+        for i in range(len(TEST_CONCEPTS)):
+            concept = TEST_CONCEPTS[i]
+            if not concept.learned:
+                outcome = concept.check_concept()
+                if len(outcome) != 0:
+                    self.failed_cons.append(concept)
+                    for j in range(len(outcome)):
+                        self.wrong_ques[i * 3 + outcome[j][0]] = outcome[j][1]
+                        # For testing
+                        print(outcome[j][1])
 
 if __name__ == "__main__":
-    with open("summary.txt", 'r') as f:
-        result = f.read()
-    c = Concept(TEST_CONCEPTS[0], result)
-    l = Lesson("heh", c)
-    l.summary = result
-    c.generate_questions()
-    print(check_concept(c))
-
+    q = Quiz()
+    q.quiz_user()
+    print(q.wrong_ques)
+    print(q.failed_cons)
 
 
 
