7cb6e289c785047cb2fc0310cc1359a597189684
YazRaso <seyedyazdanrasoulzadeh@gmail.com>
2025-03-22 21:04:42 -0400

    Added translate function

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from zipfile._path import Translator
+
 import streamlit as st
 from transformers import MT5ForConditionalGeneration, MT5Tokenizer
 import torch
@@ -6,6 +8,7 @@ import torch
 This is it! The hot stuff - Yaz
 """
 
+# Load model
 model_path = "./mt5_finetuned"
 loaded_model = MT5ForConditionalGeneration.from_pretrained(model_path)
 loaded_tokenizer = MT5Tokenizer.from_pretrained(model_path)
@@ -17,10 +20,51 @@ try:
 except Exception as e:
     raise ValueError(f"Oops, that was not supposed to happen! {e}")
 
+# Constant!
+LANGUAGE_CODES = {
+    "Wixarika": "hch",
+    "Rarámuri": "tar",
+    "Otomí": "oto",
+    "Spanish": "es",
+    "English": "en"
+}
+
+
+"""translate (input_lang, output_lang, text)
+produces the translated text from input_lang to output_lang
+"""
+
+
+def translate(input_lang, output_lang, text) -> str:
+
+    lang1 = LANGUAGE_CODES.get(input_lang)
+    lang2 = LANGUAGE_CODES.get(output_lang)
+
+    if not lang1 or not lang2:
+        raise ValueError("Both input and output languages must be defined")
+    # NB: It may be deprecated in the current version of mt5 to give input_lang and output_lang
+    input_text = f"translate {input_lang} to {output_lang}: {text}"
+
+    inputs = loaded_tokenizer(input_text, return_tensors="pt")
+
+    output_ids = loaded_model.generate(**inputs)
+
+    translated_text = loaded_tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    return translated_text
+
+
+
+
+
+
+
+
+
 st.write()
 language_options = ("Wixarika", "Rarámuri", "Otomí")
+# TODO: FIX SELECT LANGUAGE PROMPT
 st.selectbox("Select Language", language_options)
 intro = "Hola 👋 bienvenido a rAIces ☺️"
 
 # TODO: make translate function
-st.write(translate(lang=language_options, msg=intro))
+st.write(translate(lang=language_options, text=intro))
